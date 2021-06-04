fb8b94b8e43f6f5385158cd8ab7414189b2473a4
mohamed <inj.jlassi@gmail.com>
2021-06-04 17:49:34 +0100

    heroku

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const dbConnect = require("./config/dbConnect");
-require("dotenv").config();
 
 dbConnect();
 
